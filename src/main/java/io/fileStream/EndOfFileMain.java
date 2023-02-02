2c1ee00ec9394cc73bae9339177bc25d7aed96e9
LAPTOP-I8FNLLM2\user <a93151127@gmail.com>
2023-02-02 17:59:22 +0800

    tst

--- src/main/java/io/fileStream/EndOfFileMain.java
+++ src/main/java/io/fileStream/EndOfFileMain.java
@@ -20,7 +20,6 @@ public class EndOfFileMain {
             }
         } catch (EOFException e) {
             System.out.println("this is the end of file!");
-            System.out.println("this is the end of file!");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
